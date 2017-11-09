75821e438f66f5a03ce16d86098b079ae9cabb7b
saliabodian <sbodian@gmail.com>
2017-11-09 15:56:42 +0100

    Commit 40: Mise à jour du menu

--- views/sidebar.php
+++ views/sidebar.php
@@ -6,7 +6,7 @@
     <!--li> <a href="charts.php"><i class="icon icon-signal"></i> <span>Charts &amp; graphs</span></a> </li-->
     <!--li> <a href="widgets.php"><i class="icon icon-inbox"></i> <span>Widgets</span></a> </li-->
     <!--li><a href="tables.php"><i class="icon icon-th"></i> <span>Tables</span></a></li-->
-    <li class="submenu"> <a href="#"><i class="icon-cog"></i> <span>Paramètrage</span> <span class="label label-important"></span></a>
+    <!--li class="submenu"> <a href="#"><i class="icon-cog"></i> <span>Paramètrage</span> <span class="label label-important"></span></a>
       <ul>
         <li> <a href="user.php">Gestion des utilisateurs</a></li>
         <li> <a href="agence.php"> Gestion des agences</a></li>
@@ -23,7 +23,33 @@
         <li> <a href="erapport.php">Génération du erapport</a></li>
         <li> <a href="anomalieRh.php">Les anomalies</a></li>
       </ul>
+    </li-->
+    <li class="submenu"> <a href="#"><i class="icon-cogs"></i> <span>Paramètrage</span> <span class="label label-important"></span></a>
+      <ul>
+        <li> <a href="user.php"><i class="icon-user"></i><span>&nbsp;&nbsp;Gestion des utilisateurs</span></a></li>
+        <li> <a href="post.php"><i class="icon-briefcase"></i><span>&nbsp;&nbsp;Gestion des fonctions</span></a></li>
+      </ul>
     </li>
+    <li class="submenu"> <a href="#"><i class="icon-user-md"></i> <span>Ressources Humaines</span> <span class="label label-important"></span></a>
+      <ul>
+        <li> <a href="agence.php"><i class="icon-home"></i>&&nbsp;&nbsp;Gestion des agences</a></li>
+        <li> <a href="departement.php"><i class="icon-building"></i>&nbsp;&nbsp;Gestion des départements</a></li>
+        <li> <a href="qualification.php"><i class="icon-book"></i>&nbsp;&nbsp;Gestion des qualifications</a></li>
+        <li> <a href="metier.php"><i class="icon-file"></i>&nbsp;&nbsp;Gestion des métiers</a></li>
+        <li> <a href="interimaire.php"><i class="icon-group"></i>&nbsp;&nbsp;Gestion des intérimaires</a></li>
+        <li> <a href="affectation.php"><i class="icon-hand-right"></i>&nbsp;&nbsp;Ré-affectation des intérimaires</a></li>
+        <li> <a href="showAffectation.php"><i class="icon-tags"></i> &nbsp;&nbsp;Intérimaires affectés par chantier</a></li>
+      </ul>
+    </li>
+    <li class="submenu"> <a href="#"><i class="icon-eye-open"></i> <span>Contrôle de gestion</span> <span class="label label-important"></span></a>
+      <ul>
+        <li> <a href="typeTache.php"><i class="icon-list"></i>&nbsp;&nbsp;Gestion des types de tâches</a></li>
+        <li> <a href="tache.php"><i class="icon-list-alt"></i>&nbsp;&nbsp;Gestion des tâches</a></li>
+      </ul>
+    </li>
+    <li> <a href="chantier.php"><i class="icon-map-marker"></i> <span>Gestion des chantiers</span> <span class="label label-important"></span></a></li>
+    <li> <a href="erapport.php"><i class="icon-tasks"></i> <span>Génération du erapport</span> <span class="label label-important"></span></a>
+    <li> <a href="anomalieRh.php"><i class="icon-remove-sign"></i> <span>Les anomalies</span> <span class="label label-important"></span></a></li>
   </ul>
 </div>
 <!--sidebar-menu-->
