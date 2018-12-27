f556704aa337447d2a1a2fdf22f62e6b1ebbc6a4
saliabodian <sbodian@gmail.com>
2018-12-27 16:40:55 +0100

    Exemple de modèles pour l'envoi de mails

---
+++ emailTemplate.php
@@ -0,0 +1,21 @@
+<center>
+    <table  style="width: 694px; border-collapse: collapse">
+        <tbody align="center" >
+        <tr style="height: 150px; background-color: #83B817 ">
+            <!--td style="width: 174px; height: 150px; border-top-left-radius: 20px"></td-->
+            <td style="color: #ffffff;width: 523px; height: 150px; font-weight:  bolder; font-size: xx-large ; border-top-right-radius: 20px; border-top-left-radius: 20px"><img src="img/logo.png" alt="Logo de CDCL"/> - eRapport</td>
+        </tr>
+        <tr style="height: 320px">
+            <td colspan="2" style="background-color: #FFFACD; width: 697px; height: 320px; font-size:large ">
+                <p>Vous avez un certain de rapports non traités. Veillez les valider !<br>Merci de votre compréhension.</p>
+                <a href="http://summerhill.lu/~cdcl-beta/index.php" target="_blank"><div style="height: 40px;width: 30% ;border-radius: 20px; background-color: #2a00ff; color: #ffffff; alignment: center">Accéder au eRapport</div></a>
+            </td>
+        </tr>
+        <tr style="height: 150px;">
+            <td colspan="2" style="background-color: #83B817; width: 697px; height: 150px; border-bottom-left-radius: 20px; border-bottom-right-radius: 20px">
+                <div id="footer" class="span12"> 2017 &copy; eRapport By CDCL <a href="http://www.cdclux.com/en" target="_blank">www.cdclux.com</a> </div>
+            </td>
+        </tr>
+        </tbody>
+    </table>
+</center>
